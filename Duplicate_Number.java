3877281eb8e8c973008f42bc6ae772e7813937a2
Rajeev-Ranjanc <itrajeevranjan@gmail.com>
2024-03-24 11:06:17 +0530

    wrong solution

--- Duplicate_Number.java
+++ Duplicate_Number.java
@@ -1,16 +1,21 @@
 //https://leetcode.com/problems/find-the-duplicate-number/description/
 //best way to do this question is using cyclic sort
+/*
+not submitting on the leetcode
+ */
 public class Duplicate_Number {
     public static void main(String[] args) {
-        int[] nums = {1,3,4,2,2};
-        System.out.println (findDuplicate ( nums ));
+        int[] nums = {3, 3, 3, 3, 3};
+
+        System.out.println(findDuplicate(nums));
     }
+
     public static int findDuplicate(int[] nums) {
-        int num = nums.length-1;
-        int sumOfNatural = num*(num+1)/2;
+        int num = nums.length - 1;
+        int sumOfNatural = num * (num + 1) / 2;
         int arrsum = 0;
         for (int i = 0; i < nums.length; i++) {
-            arrsum+=nums[i];
+            arrsum += nums[i];
         }
         return arrsum - sumOfNatural;
     }
