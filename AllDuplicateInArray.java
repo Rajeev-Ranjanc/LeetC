25ade4d35bf3101e55742d05c7969b55ccfa5c17
Rajeev-Ranjanc <itrajeevranjan@gmail.com>
2024-03-25 11:34:08 +0530

    lil update

--- AllDuplicateInArray.java
+++ AllDuplicateInArray.java
@@ -1,11 +1,12 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+
 
 public class AllDuplicateInArray {
     public static void main(String[] args) {
-        int[] nums = {1,1,2};
+        int[] nums = {4,3,2,7,8,2,3,1};
         System.out.println(findDuplicates(nums));
     }
 
